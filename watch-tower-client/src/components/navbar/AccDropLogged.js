7b25a32aa6cb90c38ce2177d427715eef51ce8a2
Jennie R <jbiakcin@icloud.com>
2020-04-03 14:44:49 -0700

    Profile-dropdown add to nav bar

---
+++ watch-tower-client/src/components/navbar/AccDropLogged.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
+import { CURRENT_USER } from '../../graphql/queries';
+import { useApolloClient } from '@apollo/react-hooks';
+
+const AccDropLogged = () => {
+  const client = useApolloClient();
+  const { data, loading, error } = useQuery(
+    CURRENT_USER,
+    {
+      fetchPolicy: 'network-only'
+    }
+  )
+  if (loading) return <h1> Loading...</h1>
+  if (error) return <h1> Error </h1>
+  if (!data) return <h1> No user found </h1>
+  const user = data.me
+
+  return (
+    <section className="acc-drop-down">
+      <div className="acc-drop-down-header"> 
+        {user.name}
+      </div>
+      <div className="acc-drop-down-choices">
+        <Link to="/me" className="acc-drop-down-choice-button">Profile</Link>
+        <p className="acc-drop-down-choice-button"> Watch List</p>
+        <p className="acc-drop-down-choice-button"> Email & Alerts</p>
+        <p className="acc-drop-down-choice-button"> Games</p>
+      </div>
+      <div className="acc-drop-down-logout">
+        <button onClick={() => { client.resetStore() }}>Log Out</button>
+      </div>
+      
+    </section>
+  )
+}
+export default AccDropLogged;
