c3d0ab0ef1b97eeec776da4e1e61625eee1a1211
Jennie R <jbiakcin@icloud.com>
2020-04-05 16:49:27 -0700

    Password change frontend

--- watch-tower-client/src/graphql/mutations.js
+++ watch-tower-client/src/graphql/mutations.js
@@ -1,63 +1,47 @@
 import gql from 'graphql-tag';
+import { USER_CREDENTIALS_DATA, WATCHLIST_DATA } from './fragments';
 
 export const SIGNUP_USER = gql`
   mutation SignUp($email: String!, $name: String!, $password: String!) {
     signup(email: $email, name: $name, password: $password) {
-      _id
-      email
-      name
-      token
-      loggedIn
+      ...UserCredentialsData
     }
   }
+  ${USER_CREDENTIALS_DATA}
 `;
 
 export const LOGIN_USER = gql`
   mutation LogIn($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-      _id
-      email
-      name
-      token
-      loggedIn
+      ...UserCredentialsData
     }
   }
+  ${USER_CREDENTIALS_DATA}
+`;
+
+export const CHANGE_PASSWORD = gql`
+  mutation ChangePassword($oldPassword: String!, $newPassword: String!) {
+    changePassword(oldPassword: $oldPassword, newPassword: $newPassword) {
+      ...UserCredentialsData
+    }
+  }
+  ${USER_CREDENTIALS_DATA}
 `;
 
 export const ADD_WATCH_LIST_ITEM = gql`
   mutation AddWatchListItem($stockId: ID!, $addDate: String, $addPrice: Int, $noOfShares: Int) {
     addWatchListItem(stockId: $stockId, addDate: $addDate, addPrice: $addPrice, noOfShares: $noOfShares) {
-      success
-      message
-      watchList {
-        _id
-        stock {
-          _id
-          ticker
-        }
-        addDate
-        addPrice
-        noOfShares
-      }
+      ...WatchListData
     }
   }
+  ${WATCHLIST_DATA}
 `;
 
 export const REMOVE_WATCH_LIST_ITEM = gql`
   mutation RemoveWatchListItem($stockId: ID!) {
     addWatchListItem(stockId: $stockId) {
-      success
-      message
-      watchList {
-        _id
-        stock {
-          _id
-          ticker
-        }
-        addDate
-        addPrice
-        noOfShares
-      }
+      ...WatchListData
     }
   }
+  ${WATCHLIST_DATA}
 `;
