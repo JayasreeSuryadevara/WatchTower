cf4cc37fe9c780caf4b4bc46252cbe57718166c8
Jennie R <jbiakcin@icloud.com>
2020-04-04 19:49:38 -0700

    Begin watchlist show page

--- watch-tower-client/src/pages/watchlist/WatchList.js
+++ watch-tower-client/src/pages/watchlist/WatchList.js
@@ -6,7 +6,16 @@ export default () => {
 
   return (
     <main className="watch-list-main">
-      <WatchListDetails />
+      <div className="watch-list-details">
+        <WatchListDetails />
+      </div>
+      <div className="watch-list-news">
+        <h3>WATCHLIST NEWS</h3>
+        <div className="watch-list-news-list">
+          {/* This will be a link to the news later.  It is hard-coded for now. */}
+          <p>These 19 companies are working on coronavirus treatments or vaccines -- here's where things stand</p>
+        </div>
+      </div>
     </main>
   )
 }
