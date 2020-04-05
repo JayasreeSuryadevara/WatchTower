f03e93210d17fc0e9b4e103932e64b2232733c8b
JayasreeSuryadevara <sjayas@hotmail.com>
2020-04-05 14:09:13 -0700

    Adding Search feature

---
+++ watch-tower-client/src/pages/search/SearchForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SearchDetails from '../../components/search/SearchDetails';
+
+export default () => {
+    return (
+        <div>
+            <SearchDetails />
+        </div>
+    )
+}
