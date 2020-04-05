b16c14efccfe285a99fe9083ca09f61dcf9047d5
Jennie R <jbiakcin@icloud.com>
2020-04-04 19:16:08 -0700

    Add watchlist link to profile page

---
+++ watch-tower-client/src/pages/watchlist/WatchList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WatchListDetails from '../../components/watchlist/WatchListDetails';
+import '../../styles/watchlist/WatchList.css';
+
+export default () => {
+
+  return (
+    <main className="watch-list-main">
+      <WatchListDetails />
+    </main>
+  )
+}
