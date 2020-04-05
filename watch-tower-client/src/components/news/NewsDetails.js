bef29af1e32755f2c506674c71d222bc32856f2e
Philippe Fonzin <pfonzin@gmail.com>
2020-04-05 14:53:25 -0400

    fetch company specific

--- watch-tower-client/src/components/news/NewsDetails.js
+++ watch-tower-client/src/components/news/NewsDetails.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {currentNews} from '../util/CurrentNews';
+import {companyNews} from '../util/CurrentNews';
+import NewsDetailsItem from './NewsDetailsItem';
 
-export default () => {
+export default () => { 
+  const [latestNews, setLatestNews] = useState([]);
+  const [AllCompanyNews, setCompanyNews] = useState([]);
+
+  async function fetchCurrentNews() {
+    const news = await currentNews("apple")
+      setLatestNews(news) 
+  }
+
+  async function fetchCompanyNews() {
+    const news = await companyNews("tesla", "2020-04-02")
+      setCompanyNews(news) 
+  }
+
+  useEffect(() => {
+    fetchCurrentNews()
+  }, [])
+
+  useEffect(() => {
+    fetchCompanyNews()
+  }, [])
+
+  console.log(latestNews)
+  console.log(AllCompanyNews)
+
   return (
-    <h1>Hello</h1>
+      <ul>
+        {latestNews.map(article => (
+          <li key={article.title}>
+            <NewsDetailsItem title={article.title} description={article.description}
+              author={article.author} url={article.url} urlToImage={article.urlToImage} 
+              publishedAt={article.publishedAt}/>
+          </li>
+        ))}
+      </ul>
   )
 }
