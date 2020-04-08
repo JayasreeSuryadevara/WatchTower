3cd3e855b2ea4d4378a596b942d912953d4b193c
Philippe Fonzin <pfonzin@gmail.com>
2020-04-08 11:23:34 -0400

    Style home page

--- watch-tower-client/src/components/util/CurrentNews.js
+++ watch-tower-client/src/components/util/CurrentNews.js
@@ -8,6 +8,13 @@ export function currentNews() {
   )
 }
 
+export function headlineNews() {
+  const url = `https://newsapi.org/v2/everything?q="headline"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,seekingalpha.com,businessinsider.com&pageSize=50&sortBy=publishedAt&apiKey=${newsKey}`
+  return (
+    axios.get(url).then(res => res.data.articles)
+  )
+}
+
 export function companyNews(company) {
   const url = `https://newsapi.org/v2/everything?q=${company}&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,fool.com,seekingalpha.com,businessinsider.com&qInTitle=${company}&pageSize=50&sortBy=publishedAt&apiKey=${newsKey}`
   return (
