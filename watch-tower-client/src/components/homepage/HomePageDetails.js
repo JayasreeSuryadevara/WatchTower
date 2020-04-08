4079c7c4d1dea8ff617ad86e0a15b0c0466e0de6
Philippe Fonzin <pfonzin@gmail.com>
2020-04-08 11:23:34 -0400

    Style home page

--- watch-tower-client/src/components/homepage/HomePageDetails.js
+++ watch-tower-client/src/components/homepage/HomePageDetails.js
@@ -1,32 +1,54 @@
 import React, { useEffect, useState } from "react";
 import '../../styles/homePage/HomePage.css';
-import { currentNews } from '../util/CurrentNews';
+import { headlineNews } from '../util/CurrentNews';
 import NewsDetailsItem from '../news/NewsDetailsItem';
+import '../../styles/homePage/HomePageDetails.css';
 
 export default () => {
-  const [latestNews, setLatestNews] = useState([]);
+  const [headlines, setHeadlineNews] = useState([]);
 
-  async function fetchCurrentNews() {
-    const news = await currentNews()
-    setLatestNews(news)
+  async function fetchHeadlineNews() {
+    const news = await headlineNews()
+    setHeadlineNews(news)
   }
 
   useEffect(() => {
-    fetchCurrentNews()
+    fetchHeadlineNews()
   }, [])
 
+  const items = headlines.map(article => {
+    const date = article.publishedAt.slice(0, 10);
+    const time = article.publishedAt.slice(11, 16);
+    return (
+      <NewsDetailsItem title={article.title} description={article.description}
+        author={article.author} url={article.url} urlToImage={article.urlToImage}
+        publishedAt={date} time={time} key={headlines.indexOf(article)} />
+    )
+  })
+
   return(
-    <div className="main-page">
-      <div className="newsContainer">
-        <ul>
-          {latestNews.map(article => (
-            <li key={latestNews.indexOf(article)}>
-              <NewsDetailsItem title={article.title} description={article.description}
-                author={article.author} url={article.url} urlToImage={article.urlToImage}
-                publishedAt={article.publishedAt} />
-            </li>
-          ))}
-        </ul>
+    <div className="main-page-container">
+      <div className="main-page">
+        <div className="graph-header-container">
+          <div className="graph-header-items">
+            <div className="graph-header-item">US</div>
+            <div className="graph-header-item">Euro</div>
+            <div className="graph-header-item">Asia</div>
+            <div className="graph-header-item">FX</div>
+            <div className="graph-header-item">Rates</div>
+            <div className="graph-header-item">Futures</div>
+            <div className="graph-header-item">Crypto</div>
+          </div>
+          <div className="movers-latest-items">
+            <p className="movers-latest-item">S&P 500 Movers</p>
+            <p className="movers-latest-item">Latest News</p>
+          </div>
+        </div>
+        <div className="graph-container">
+          <div className="graph">
+            <p>graph</p>
+          </div>
+        </div>
       </div>
     </div>
   )
