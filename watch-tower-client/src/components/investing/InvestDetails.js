7d4daf90198d550a7d8530deb28ce025d449c561
JayasreeSuryadevara <sjayas@hotmail.com>
2020-04-10 10:29:04 -0700

    Rename StackQuotes

--- watch-tower-client/src/components/investing/InvestDetails.js
+++ watch-tower-client/src/components/investing/InvestDetails.js
@@ -3,7 +3,7 @@ import { useQuery, useMutation } from '@apollo/react-hooks';
 // import yahooQuote from '../util/StockQuotes';
 // import updateHistData from '../util/updateHistData';
 import { ALL_STOCKS, GET_HISTORICAL_DATA } from '../../graphql/queries';
-import fetchStockData from '../util/FmpQuotes';
+import stockQuotes from '../util/StockQuotes';
 // import { UPDATE_HISTORICAL_DATA, ADD_HISTORICAL_DATA } from '../../graphql/mutations';
 
 export default () => {
@@ -14,7 +14,7 @@ export default () => {
   if(!data) return <p> Not Found </p>;
   
   function handleQuote() {
-    const stocks = fetchStockData().then((response) => console.log(response))
+    const stocks = stockQuotes().then((response) => console.log(response))
   }
 
   return (
