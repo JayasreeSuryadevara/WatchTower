eefc22de206271a1a4e62f4cd41e38210f5dd049
Philippe Fonzin <pfonzin@gmail.com>
2020-04-10 12:45:56 -0400

    Create key data item

---
+++ watch-tower-client/src/components/ticker/KeyDataItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../styles/ticker/KeyDataItem.css';
+
+const KeyDataItem = (props) => {
+  <div className="kd-main-contianer">
+    <div className="data-container">
+      <div className="kd-title-container">
+        <span>Open</span>
+      </div>
+      <div className="kd-value-container">
+        <span>$123.45</span>
+      </div>
+    </div>
+  </div>
+}
+
+export default KeyDataItem;
