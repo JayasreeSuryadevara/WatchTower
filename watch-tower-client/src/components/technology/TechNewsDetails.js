9fdbf9cd17c7d3bf5205f696a5a45fef741a2b9b
Jennie R <jbiakcin@icloud.com>
2020-04-11 13:13:00 -0700

    Add stock display bar on technology page

--- watch-tower-client/src/components/technology/TechNewsDetails.js
+++ watch-tower-client/src/components/technology/TechNewsDetails.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { currentNews } from '../util/CurrentNews';
 import { technologyNews } from '../util/CurrentNews';
 import TechNewsDetailsItem from './TechNewsDetailsItem';
 import { Link } from 'react-router-dom';
 import '../../styles/news/newsDetails.css';
+import fetchStockData from '../util/StockQuotes';
 
 export default () => {
   const [allTechnologyNews, setTechnologyNews] = useState([]);
+  const [techStockData, setTechStockData] = useState([]);
 
   async function fetchTechnologyNews() {
     const news = await technologyNews()
@@ -17,7 +18,15 @@ export default () => {
     fetchTechnologyNews()
   }, [])
 
-  const items = allTechnologyNews.slice(0, 19).map(article => {
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetchStockData("CSCO, NFLX, ZM, AMZN, HPE, GOOGL")
+      setTechStockData(response)
+    }
+    fetchData()
+  }, []);
+
+  const items = allTechnologyNews.slice(0, 14).map(article => {
     const date = article.publishedAt.slice(0, 10);
     const time = article.publishedAt.slice(11, 16);
     return (
@@ -27,11 +36,37 @@ export default () => {
     )
   })
 
+  const formatter = new Intl.NumberFormat('en-Us', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
   return (
     <div>
       <div className="tech-news-page-title">Technology News</div>
       <div className="tech-news-page-home">
         <Link to="/" className="tech-news-page-home-link">Home ></Link> Technology
+      </div>
+      <div className="tech-news-stock-info-panel">
+        <ul className="tech-news-stock-list">
+          {techStockData.map((stock, i) => {
+            const ticker = stock.symbol;
+            const price = stock.price;
+            return <div key={i} className="tech-news-stock">
+              <p className="tech-news-stock-ticker">{ticker}</p>
+              <p>{formatter.format(price)}</p>
+            </div>
+          })}
+        </ul>
+      </div>
+      <div className="tech-news-newsviewer">
+        <div className="newsviewer-header">
+          <h2>NEWSVIEWER</h2>
+          <div className="newsviewer-bar">  </div>
+        </div>
+        <div>
+          <p className="newsviewer-real-time">Real-time updates from WatchTower</p>
+        </div>
       </div>
       <ul>
         {items}
