5859fb3f08049b3e7553b0230c3a48d0ea53c753
JayasreeSuryadevara <sjayas@hotmail.com>
2020-04-12 14:09:50 -0700

    Added util for historical and markets page details

--- watch-tower-client/src/components/util/HistoricalQuotes.js
+++ watch-tower-client/src/components/util/HistoricalQuotes.js
@@ -1,6 +1,5 @@
-export async function dayActives () {
-  // Returns 10 most active stocks for the day
-  const API_CALL = "https://financialmodelingprep.com/api/v3/stock/actives"
+export async function quarterHourHistory(symbol) {
+  const API_CALL = "https://financialmodelingprep.com/api/v3/historical-chart/15min/" + symbol;
 
   return await fetch(API_CALL)
     .then(
@@ -22,9 +21,8 @@ export async function dayActives () {
     );
 };
 
-export async function dayGainers() {
-  // Returns 10 highest gainer stocks for the day
-  const API_CALL = "https://financialmodelingprep.com/api/v3/stock/gainers"
+export async function halfHourHistory(symbol) {
+  const API_CALL = "https://financialmodelingprep.com/api/v3/historical-chart/30min/" + symbol;
 
   return await fetch(API_CALL)
     .then(
@@ -46,9 +44,8 @@ export async function dayGainers() {
     );
 };
 
-export async function dayLosers() {
-  // Returns 10 highest loosing stocks for the day
-  const API_CALL = "https://financialmodelingprep.com/api/v3/stock/losers"
+export async function hourlyHistory(symbol) {
+  const API_CALL = "https://financialmodelingprep.com/api/v3/historical-chart/1hour/" + symbol
 
   return await fetch(API_CALL)
     .then(
@@ -69,3 +66,28 @@ export async function dayLosers() {
       err => { throw new Error(err); }
     );
 };
+
+export async function dayHistory(symbol) {
+  const API_CALL = "https://financialmodelingprep.com/api/v3/historical-price-full/index/" + symbol
+
+  return await fetch(API_CALL)
+    .then(
+      (res) => { return res.json(); },
+      err => { throw new Error(err); }
+    )
+    .then(
+      (data) => {
+        if (data) {
+          return data;
+        } else {
+          return null
+        }
+      },
+      err => { throw new Error(err); }
+    )
+    .catch(
+      err => { throw new Error(err); }
+    );
+};
+
+
