92d1eceaff748797628f4e9d9e96ed134c1c3ea2
JayasreeSuryadevara <sjayas@hotmail.com>
2020-04-12 17:08:20 -0700

    merge search results

--- watch-tower-client/src/components/search/SearchListItem.js
+++ watch-tower-client/src/components/search/SearchListItem.js
@@ -1,7 +1,6 @@
 import React,{ useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import searchStock from '../util/SearchStock';
-import TickerPAge from ''
 
 const SearchListItem = ({ticker}) => {
 
