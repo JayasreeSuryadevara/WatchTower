b6a2a152596ebe32de859007932d88b6e542bb25
JayasreeSuryadevara <sjayas@hotmail.com>
2020-04-29 16:05:54 -0700

    Environment variable API Key error

--- watch-tower-client/src/components/util/CurrentNews.js
+++ watch-tower-client/src/components/util/CurrentNews.js
@@ -1,38 +1,37 @@
 const axios = require("axios"); 
 // const newsKey = require('../../config/keys').NewsApiKey;
-const newsKey = process.env.NewsApiKey;
+// const newsKey = process.env.NewsApiKey;
 
 export function currentNews() {
-  console.log("News API Key :", newsKey);
-  const url = `https://newsapi.org/v2/everything?q="stock market"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,seekingalpha.com,businessinsider.com&pageSize=50&sortBy=publishedAt&apiKey=${newsKey}`
+  const url = `https://newsapi.org/v2/everything?q="stock market"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,seekingalpha.com,businessinsider.com&pageSize=50&sortBy=publishedAt&apiKey=${process.env.NewsApiKey}`
   return (
     axios.get(url).then(res => res.data.articles)
   )
 }
 
 export function headlineNews() {
-  const url = `https://newsapi.org/v2/everything?q="headline"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,seekingalpha.com,businessinsider.com&pageSize=50&sortBy=publishedAt&apiKey=${newsKey}`
+  const url = `https://newsapi.org/v2/everything?q="headline"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,seekingalpha.com,businessinsider.com&pageSize=50&sortBy=publishedAt&apiKey=${process.env.NewsApiKey}`
   return (
     axios.get(url).then(res => res.data.articles)
   )
 }
 
 export function companyNews(company) {
-  const url = `https://newsapi.org/v2/everything?q=${company}&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,seekingalpha.com,businessinsider.com&qInTitle=${company}&pageSize=15&sortBy=publishedAt&apiKey=${newsKey}`
+  const url = `https://newsapi.org/v2/everything?q=${company}&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,seekingalpha.com,businessinsider.com&qInTitle=${company}&pageSize=15&sortBy=publishedAt&apiKey=${process.env.NewsApiKey}`
   return (
     axios.get(url).then(res => res.data.articles)
   )
 }
 
 export function technologyNews() {
-  const url = `https://newsapi.org/v2/everything?q="technology stocks"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,fool.com,seekingalpha.com,businessinsider.com,cnn.com&pageSize=50&sortBy=publishedAt&apiKey=${newsKey}`
+  const url = `https://newsapi.org/v2/everything?q="technology stocks"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,fool.com,seekingalpha.com,businessinsider.com,cnn.com&pageSize=50&sortBy=publishedAt&apiKey=${process.env.NewsApiKey}`
   return (
     axios.get(url).then(res => res.data.articles)
   )
 }
 
 export function principlesInvestingNews() {
-  const url = `https://newsapi.org/v2/everything?q="investing principles"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,fool.com,seekingalpha.com,businessinsider.com,cnn.com&pageSize=50&sortBy=publishedAt&apiKey=${newsKey}`
+  const url = `https://newsapi.org/v2/everything?q="investing principles"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,fool.com,seekingalpha.com,businessinsider.com,cnn.com&pageSize=50&sortBy=publishedAt&apiKey=${process.env.NewsApiKey}`
   return (
     axios.get(url).then(res => res.data.articles)
   )
