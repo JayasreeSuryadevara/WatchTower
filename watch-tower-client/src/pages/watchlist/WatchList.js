296f0875c3e5c848060aa572beb06d49e0df7f80
Jennie R <jbiakcin@icloud.com>
2020-05-20 21:06:38 -0700

    Watchlist resize css

--- watch-tower-client/src/pages/watchlist/WatchList.js
+++ watch-tower-client/src/pages/watchlist/WatchList.js
@@ -7,20 +7,20 @@ export default () => {
 
   return (
     <main className="watch-list-main">
-      <header>
-        <h1>Watchlist</h1>
-      </header>
-      <section className="watch-list-body">
-        <div className="watch-list-details">
-          <WatchListDetails />
-        </div>
-        <div className="watch-list-news">
-          <h3 className="watch-list-news-header">LATEST NEWS</h3>
-          <div className="watch-list-news-list">
-            <WatchListLatestNews/>
+        <header>
+          <h1>Watchlist</h1>
+        </header>
+        <section className="watch-list-body">
+          <div className="watch-list-details">
+            <WatchListDetails />
           </div>
-        </div>
-      </section>
+          <div className="watch-list-news">
+            <h3 className="watch-list-news-header">LATEST NEWS</h3>
+            <div className="watch-list-news-list">
+              <WatchListLatestNews/>
+            </div>
+          </div>
+        </section>
     </main>
   )
 }
