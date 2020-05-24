6ddb5b503299cba49c6d83abf98e47024377d271
JayasreeSuryadevara <sjayas@hotmail.com>
2020-05-24 12:59:34 -0700

    Added cors-anywhere proxy to news API calls

--- watch-tower-client/src/components/util/CurrentNews.js
+++ watch-tower-client/src/components/util/CurrentNews.js
@@ -1,37 +1,38 @@
 const axios = require("axios"); 
 const newsKey = require('../../config/keys').NewsApiKey;
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
 export function currentNews() {
-  const url = `/everything?q="stock market"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,businessinsider.com&pageSize=50&sortBy=publishedAt&apiKey=${newsKey}`
+  const url = `https://newsapi.org/v2/everything?q="stock market"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,businessinsider.com&pageSize=50&sortBy=publishedAt&apiKey=${newsKey}`
   return (
-    axios.get(url).then(res => res.data.articles)
+    axios.get(proxyurl + url).then(res => res.data.articles)
   )
 }
 
 export function headlineNews() {
-  const url = `/everything?q="headline"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,businessinsider.com&pageSize=50&sortBy=publishedAt&apiKey=${newsKey}`
+  const url = `https://newsapi.org/v2/everything?q="headline"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,businessinsider.com&pageSize=50&sortBy=publishedAt&apiKey=${newsKey}`
   return (
-    axios.get(url).then(res => res.data.articles)
+    axios.get(proxyurl + url).then(res => res.data.articles)
   )
 }
 
 export function companyNews(company) {
-  const url = `/everything?q=${company}&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,businessinsider.com&qInTitle=${company}&pageSize=15&sortBy=publishedAt&apiKey=${newsKey}`
+  const url = `https://newsapi.org/v2/everything?q=${company}&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,businessinsider.com&qInTitle=${company}&pageSize=15&sortBy=publishedAt&apiKey=${newsKey}`
   return (
-    axios.get(url).then(res => res.data.articles)
+    axios.get(proxyurl + url).then(res => res.data.articles)
   )
 }
 
 export function technologyNews() {
-  const url = `/everything?q="technology stocks"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,fool.com,seekingalpha.com,businessinsider.com,cnn.com&pageSize=50&sortBy=publishedAt&apiKey=${newsKey}`
+  const url = `https://newsapi.org/v2/everything?q="technology stocks"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,fool.com,seekingalpha.com,businessinsider.com,cnn.com&pageSize=50&sortBy=publishedAt&apiKey=${newsKey}`
   return (
-    axios.get(url).then(res => res.data.articles)
+    axios.get(proxyurl + url).then(res => res.data.articles)
   )
 }
 
 export function principlesInvestingNews() {
-  const url = `/everything?q="investing principles"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,fool.com,seekingalpha.com,businessinsider.com,cnn.com&pageSize=50&sortBy=publishedAt&apiKey=${newsKey}`
+  const url = `https://newsapi.org/v2/everything?q="investing principles"&language=en&domains=wsj.com,nytimes.com,cnbc.com,foxbusiness.com,fool.com,seekingalpha.com,businessinsider.com,cnn.com&pageSize=50&sortBy=publishedAt&apiKey=${newsKey}`
   return (
-    axios.get(url).then(res => res.data.articles)
+    axios.get(proxyurl + url).then(res => res.data.articles)
   )
 }
