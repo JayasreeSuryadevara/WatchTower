1e2a61bf543665cb41dcf41f52b0ac54d95a7778
Philippe Fonzin <pfonzin@gmail.com>
2020-06-05 13:28:43 -0400

    Change build

--- watch-tower-server/index.js
+++ watch-tower-server/index.js
@@ -45,8 +45,8 @@ app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
   app.use(express.static('build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   })
 }
 
