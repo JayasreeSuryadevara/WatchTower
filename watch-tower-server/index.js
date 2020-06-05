2a0e35d37ee33cfc9342601b42ad56e39a5de83f
Philippe Fonzin <pfonzin@gmail.com>
2020-06-05 13:50:54 -0400

    Change build

--- watch-tower-server/index.js
+++ watch-tower-server/index.js
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === 'production') {
   const path = require('path');
   app.use(express.static('build'));
   app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
   })
 }
 
